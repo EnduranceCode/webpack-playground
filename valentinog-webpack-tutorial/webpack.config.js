8c67a81dd8f332e92f46425ddaad18439c35a681
EnduranceCode <endurancecode@gmail.com>
2021-10-28 19:49:44 +0100

    ValentinoG - Setup webpack-development-server
    https://www.valentinog.com/blog/webpack/#webpacks-development-server

--- valentinog-webpack-tutorial/webpack.config.js
+++ valentinog-webpack-tutorial/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   // The Entry point (https://www.valentinog.com/blog/webpack/#entry-point) is being set here with webpack's default
   // and therefore wouldn't need to be explicitly set. It is only being set to be used as an example how it can be customized.
   entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+
   // The Output (https://www.valentinog.com/blog/webpack/#output) is being set here with webpack's default
   // and therefore wouldn't need to be explicitly set. It is only being set to be used as an example how it can be customized.
   output: {
@@ -16,4 +17,8 @@ module.exports = {
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
+
+  devServer: {
+    open: true,
+  },
 };
