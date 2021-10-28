454ffcaf740cbd13f8ba5f5c0c17ca7fa92212fc
EnduranceCode <endurancecode@gmail.com>
2021-10-28 19:14:04 +0100

    ValentinoG - Setup webpack configuration file
    https://www.valentinog.com/blog/webpack/#configuring-webpack

---
+++ valentinog-webpack-tutorial/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  // The Entry point (https://www.valentinog.com/blog/webpack/#entry-point) is being set here with webpack's default
+  // and therefore wouldn't need to be explicitly set. It is only being set to be used as an example how it can be customized.
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  // The Output (https://www.valentinog.com/blog/webpack/#output) is being set here with webpack's default
+  // and therefore wouldn't need to be explicitly set. It is only being set to be used as an example how it can be customized.
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
