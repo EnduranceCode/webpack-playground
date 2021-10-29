1552a49d4de758cb2b2cddaf74a477dd04fda1e0
EnduranceCode <endurancecode@gmail.com>
2021-10-29 22:17:43 +0100

    ValentinoG - Extract CSS files with MiniCssExtractPlugin

--- valentinog-webpack-tutorial/webpack.config.js
+++ valentinog-webpack-tutorial/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: { index: path.resolve(__dirname, 'src', 'js', 'script.js') },
 
   output: {
     clean: true,
-    filename: 'js/main.js',
+    filename: 'js/script.js',
     path: path.resolve(__dirname, 'dist'),
   },
 
@@ -14,7 +15,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -22,6 +23,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
     }),
   ],
 
