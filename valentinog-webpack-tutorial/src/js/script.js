12096abf50d8415d35220f0616fb44558be7e5f6
EnduranceCode <endurancecode@gmail.com>
2021-10-30 11:38:44 +0100

    ValentinoG - Use code splitting with Dynamic Imports
    https://www.valentinog.com/blog/webpack/#code-splitting-with-dynamic-imports

--- valentinog-webpack-tutorial/src/js/script.js
+++ valentinog-webpack-tutorial/src/js/script.js
@@ -1,2 +1,13 @@
+import '../css/style.scss';
+
 // eslint-disable-next-line no-unused-vars
 import moment from 'moment';
+const getUserModule = () => import(/* webpackChunkName: "usersAPI" */ '../common/usersAPI');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  getUserModule().then(({ getUsers }) => {
+    getUsers().then((json) => console.log(json));
+  });
+});
